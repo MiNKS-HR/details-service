05ad694ee69f1111e531c41d544726dc7a857ffa
mabrsweeney <mabrsweeney@gmail.com>
2018-02-22 11:19:41 -0800

    airbnb style

--- db/seed.js
+++ db/seed.js
@@ -1,36 +1,37 @@
 const mongoose = require('mongoose');
-const mock =require('./mock.json');
+const mock = require('./mock.json');
 
 mongoose.connect('mongodb://127.0.0.1/experiences');
-var Schema = mongoose.Schema;
-var modelSchema = new Schema( {
+
+const { Schema } = mongoose;
+const modelSchema = new Schema({
   id: Number,
-  experience_title: String,
-  host_name: String,
   experience_category: String,
+  lat: Number,
   host_picture_url: String,
-  amenities: String,
-  duration: Number,
+  notes: String,
+  what_well_do: String,
   language: String,
   host_about: String,
-  what_well_do: String,
-  what_ill_provide: String,
+  host_name: String,
+  amenities: String,
   who_can_come: String,
-  notes: String,
-  lat: Number,
-  long: Number
-} );
+  duration: Number,
+  what_ill_provide: String,
+  long: Number,
+  experience_title: String,
+});
 
 
 // Checks to see if the collection has any items and removes them
 // before adding new items from mock.json
-var Detail = mongoose.model('Detail', modelSchema);
+const Detail = mongoose.model('Detail', modelSchema);
 Detail.find({}).remove(() => {
-  Detail.create(mock, ( err ) => {
-    if ( err ) throw err;
-    console.log( 'worked!' );
-    db.close();
-  })
+  Detail.create(mock, (err) => {
+    if (err) throw err;
+    console.log('Database seeded!');
+    mongoose.connection.close();
+  });
 });
 
 module.exports = modelSchema;
