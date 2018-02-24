7b652fc9c78b1aed22319e65f877412e4e99a198
mabrsweeney <mabrsweeney@gmail.com>
2018-02-23 19:42:48 -0800

    removed old test file

--- testing/test.js
+++
@@ -1,8 +0,0 @@
-import renderer from 'react-test-renderer';
-
-test('should render details', () => {
-  const component = renderer.create('<Details />');
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
-});
-
