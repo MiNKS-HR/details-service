a7520b8e15cb4d929581a555f3a2f8693ba0b04e
mabrsweeney <mabrsweeney@gmail.com>
2018-02-26 12:00:30 -0800

    now passes props in

--- client/app.js
+++ client/app.js
@@ -2,4 +2,29 @@ import ReactDOM from 'react-dom';
 import React from 'react';
 import Details from './details.jsx';
 
-ReactDOM.render(<Details />, document.getElementById('details'));
+ReactDOM.render(<Details 
+  details={{
+    id: 0,
+    location: {
+      lat: 0.0,
+      long: 0.0,
+    },
+    host: {
+      name: '',
+      about: '',
+      picture_url: '',
+    },
+    experience: {
+      category: '',
+      title: '',
+    },
+    notes: '',
+    language: '',
+    duration: 0,
+    city: '',
+    view_count: 0,
+    spots_left: 0,
+    what_well_do: '',
+    who_can_come: '',
+    what_ill_provide: ''}}
+/>, document.getElementById('details'));
