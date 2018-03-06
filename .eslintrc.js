06e9ec00dad29a155ef77f57cc09ab45f01152e0
mabrsweeney <mabrsweeney@gmail.com>
2018-03-05 18:46:34 -0800

    extensions rule appeased

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,6 @@ module.exports = {
     "rules": {
         "jsx-a11y/click-events-have-key-events": "off",
         "jsx-a11y/no-static-element-interactions": "off",
-        "import/extensions": "off",
         "react/prop-types": "off",
         "no-console": "off",
         "no-undef": "off",
