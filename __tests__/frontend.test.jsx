246f259c63b0e0286bf389df9a12967bf1606e3e
mabrsweeney <mabrsweeney@gmail.com>
2018-03-05 18:47:19 -0800

    converted to jsx

---
+++ __tests__/frontend.test.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import { mount, configure } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import sinon from 'sinon';
+import Details from '../client/components/details';
+import testDetails from '../db/testDetails';
+
+configure({ adapter: new Adapter() });
+
+describe('Front end rendering tests', () => {
+  const wrapper = mount(<Details details={testDetails} />);
+  it('should render Details without throwing an error', () => {
+    expect(wrapper.exists());
+  });
+
+  it('should render Map without throwing an error', () => {
+    expect(wrapper.find('#map-container')).toHaveLength(1);
+  });
+
+  it('should render Para without throwing an error', () => {
+    expect(wrapper.find('.para')).toHaveLength(4);
+    expect(wrapper.find('.cond-para')).toHaveLength(1);
+  });
+});
+
+describe('Front end clicking tests,', () => {
+  const wrapper = mount(<Details details={testDetails} />);
+  it('should expand paragraph on click', () => {
+    expect(wrapper.find('.cond-para')).toHaveLength(1);
+    wrapper.find('.more').simulate('click');
+    expect(wrapper.find('.cond-para')).toHaveLength(0);
+  });
+
+  it('should call getHost method', () => {
+    const spy = jest.spyOn(Details.prototype, 'getHost');
+    wrapper.find('.get-host').simulate('click');
+    expect(spy).toHaveBeenCalled();
+  });
+});
