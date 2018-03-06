2bab42a521f0e4c3dee93e6b09c24f41b58c5767
mabrsweeney <mabrsweeney@gmail.com>
2018-03-06 15:10:07 -0800

    added styles for proxy

--- client/components/details.jsx
+++ client/components/details.jsx
@@ -85,6 +85,7 @@ class Details extends React.Component {
               ${new Date().getTime()}`} // In order for the gif to refresh we need to add the time as a parameter
             />
           </div>
+          <hr className="separate" />
         </div>);
     }
     return (<div />);
@@ -96,17 +97,20 @@ class Details extends React.Component {
     const left = this.state.spots_left;
     if (left < 10 && left !== 0) {
       return (
-        <div className="align">
-          <p className="count">There are only a few spots left. Join {left} others
-      at Jacob And Jessica’s experience next Tuesday.
-          </p>
-          <div className="buffer" />
-          <img
-            className="gif"
-            alt="clock"
-            src={`https://a0.muscache.com/airbnb/static/page2/icon-number-available-animated-074b392fde3b450acdc18e531e56ce61.gif?
-              ${new Date().getTime()}`}
-          />
+        <div>
+          <div className="align">
+            <p className="count">There are only a few spots left. Join {left} others
+        at Jacob And Jessica’s experience next Tuesday.
+            </p>
+            <div className="buffer" />
+            <img
+              className="gif"
+              alt="clock"
+              src={`https://a0.muscache.com/airbnb/static/page2/icon-number-available-animated-074b392fde3b450acdc18e531e56ce61.gif?
+                ${new Date().getTime()}`}
+            />
+          </div>
+          <hr className="separate" />
         </div>);
     }
     return (<div />);
@@ -142,48 +146,57 @@ class Details extends React.Component {
               <img className="host-picture" alt="Host" src={this.state.host.picture_url} />
             </div>
           </div>
-          <ul>
-            <li>
-              <div
-                className="clickable show-map"
-                onClick={() => {
-              $('html, body').animate({
-                scrollTop: $('#map-container').offset().top,
-              }, 1000);
-            }}
-              >{this.state.city}
-              </div>
-            </li>
-            <li>{this.state.duration} hours long</li>
-            <li>Offered in {this.state.language}</li>
-          </ul>
+          <hr className="separate" />
+          <div className="section">
+            <ul className="info-list">
+              <li>
+                <div
+                  className="clickable show-map"
+                  onClick={() => {
+                $('html, body').animate({
+                  scrollTop: $('#map-container').offset().top,
+                }, 1000);
+              }}
+                >{this.state.city}
+                </div>
+              </li>
+              <li>{this.state.duration} hours long</li>
+              <li>Offered in {this.state.language}</li>
+            </ul>
+          </div>
         </div>
+        <hr className="separate" />
         {this.viewCount()}
         {this.spotsLeft()}
         <div className="about section">
-          <h3>{`About your host, ${this.state.host.name}`}</h3>
+          <h3 className="sub-header"> {`About your host, ${this.state.host.name}`}</h3>
           <Para p={this.state.host.about} />
         </div>
+        <hr className="separate" />
         <div className="about section">
-          <h3>{'What we\'ll do'}</h3>
+          <h3 className="sub-header"> {'What we\'ll do'}</h3>
           <Para p={this.state.what_well_do} />
         </div>
+        <hr className="separate" />
         <div className="provide section">
-          <h3>{'What I\'ll provide'}</h3>
+          <h3 className="sub-header"> {'What I\'ll provide'}</h3>
           <Para p={this.state.what_ill_provide} />
-
         </div >
+        <hr className="separate" />
         <div className="who section">
-          <h3>Who can come</h3>
+          <h3 className="sub-header"> Who can come</h3>
           <Para p={this.state.who_can_come} />
-
         </div>
+        <hr className="separate" />
         <div className="notes section">
-          <h3>Notes</h3>
+          <h3 className="sub-header"> Notes</h3>
           <Para p={this.state.notes} />
         </div>
-        <h3>{'Where we\'ll be'}</h3>
-        {this.latLngBounds()}
+        <hr className="separate" />
+        <div className="gmaps section">
+          <h3 className="sub-header"> {'Where we\'ll be'}</h3>
+          {this.latLngBounds()}
+        </div>
       </div>);
   }
 }
