e0efce8f4e09b24f99bd2dbda641347f412b2297
mabrsweeney <mabrsweeney@gmail.com>
2018-03-05 18:46:56 -0800

    added public path

--- webpack.config.js
+++ webpack.config.js
@@ -4,10 +4,14 @@ const SRC_DIR = path.join(__dirname, '/client');
 const DIST_DIR = path.join(__dirname, '/public');
 
 module.exports = {
-  entry: `${SRC_DIR}/app.js`,
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  entry: `${SRC_DIR}/app.jsx`,
   output: {
     filename: 'bundle.js',
     path: DIST_DIR,
+    publicPath: '/',
   },
   module: {
     loaders: [
@@ -16,7 +20,7 @@ module.exports = {
         include: SRC_DIR,
         loader: 'babel-loader',
         query: {
-          presets: ['react', 'es2015'],
+          presets: ['react', 'env'],
         },
       },
       {
